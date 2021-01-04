6cdb37fed8c0393a6b88c95d089be77823659339
Gweezdo <z.trivium@gmail.com>
2021-01-04 19:19:39 +1100

    updated menu-items to navigate with "withRouter" higher order component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,28 +14,33 @@ class Directory extends React.Component {
               title: "hats",
               imageURL: "https://i.ibb.co/cvpntL1/hats.png",
               id: 1,
+              linkUrl: "hats",
             },
             {
               title: "jackets",
               imageURL: "https://i.ibb.co/px2tCc3/jackets.png",
               id: 2,
+              linkUrl: "",
             },
             {
               title: "sneakers",
               imageURL: "https://i.ibb.co/0jqHpnp/sneakers.png",
               id: 3,
+              linkUrl: "",
             },
             {
               title: "women",
               imageURL: "https://i.ibb.co/GCCdy8t/womens.png",
               size: "large",
               id: 4,
+              linkUrl: "",
             },
             {
               title: "men",
               imageURL: "https://i.ibb.co/R70vBrQ/men.png",
               size: "large",
               id: 5,
+              linkUrl: "",
             },
           ],
         };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
     render(){
         return (
           <div className="directory-menu">
-            {this.state.sections.map(({ title, imageURL, id, size }) => (
-              <MenuItem title={title} imageURL={imageURL} key={id} size={size}/>
+            {this.state.sections.map(({ id, ...otherSectionProps}) => (
+              <MenuItem key={id} {...otherSectionProps}/>
             ))}
           </div>
         );
