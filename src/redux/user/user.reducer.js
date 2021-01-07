5958588c7f4258e1640db50afffd423007789b21
Gweezdo <z.trivium@gmail.com>
2021-01-08 10:14:17 +1100

    updating app to redirect to home if user is signed in.

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
 	currentUser: null
 }
 const userReducer = (state = INITIAL_STATE, action) => {  //ES6 ability to set a default value on a function parameter. If state is ever undefined, default value (INITIAL_STATE) will trigger. Remember 'null' is a valid value.
 //Note: Every single reducer gets every single action that ever get fired, even if the actions are not related to this reducer
 	switch(action.type) {
-		case 'SET_CURRENT_USER':
+		case UserActionTypes.SET_CURRENT_USER:
 			return {
 				...state,
 				currentUser: action.payload
