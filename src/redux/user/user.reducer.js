0fe28ad46fe86f462f366ea43466ba71b9460276
Gweezdo <z.trivium@gmail.com>
2021-01-08 09:53:44 +1100

    Added Redux to app and implemented userReducer and userActions to header and app component

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+	currentUser: null
+}
+const userReducer = (state = INITIAL_STATE, action) => {  //ES6 ability to set a default value on a function parameter. If state is ever undefined, default value (INITIAL_STATE) will trigger. Remember 'null' is a valid value.
+//Note: Every single reducer gets every single action that ever get fired, even if the actions are not related to this reducer
+	switch(action.type) {
+		case 'SET_CURRENT_USER':
+			return {
+				...state,
+				currentUser: action.payload
+			}
+
+		default:
+			return state;
+	}
+}	
+
+export default userReducer;
